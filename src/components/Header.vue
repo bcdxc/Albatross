edb83f37476cb5a3943077fa8ad543ecdc4eeb34
bcd_xc <xchaoyunlai@163.com>
2021-05-16 10:34:56 +0800

    项目第二次更新

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
         <div class="head-left">
             <router-link class="logo" to="/">
                 <img src="../assets/images/logo2.png" alt="" />
-                <span>Albatross</span>
+                <span>Albatross111</span>
             </router-link>
         </div>
         <div class="head-right" v-if="!$route.path.includes('login')">
